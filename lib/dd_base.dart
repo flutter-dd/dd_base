5052649873119d32980fb5604e96520338e903b6
duanchnaghe <592110272@qq.com>
2021-01-08 15:04:03 +0800

    update

--- lib/dd_base.dart
+++ lib/dd_base.dart
@@ -4,7 +4,6 @@ import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 part 'src/BasePage.dart';
 part 'src/dd_extension.dart';
 part 'src/dd_extension_ui.dart';
